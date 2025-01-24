edfc27ada416032b838fe4df6c8e5ad513100473
shibil <mhdshibil859@gmail.com>
2025-01-24 14:03:35 +0530

    ad

--- leadapp/forms.py
+++ leadapp/forms.py
@@ -1,10 +1,14 @@
 from leadapp.models import LEAD
+
 from django import forms
 
-class LeadForm(forms.Modelform):
+class LeadForm(forms.ModelForm):
 
     class Meta:
 
+
+
         model=LEAD
+
         fields="__all__"
 
